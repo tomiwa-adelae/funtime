1485ad0c15d5a416fcf48299a2a2e48bfd00b36e
Tomiwa Adelae <thetommedia@gmail.com>
2024-09-24 07:54:34 +0100

    Updated the shapes and height

--- components/LettersInput.tsx
+++ components/LettersInput.tsx
@@ -12,14 +12,14 @@ const LettersInput = () => {
 		<div className="w-full flex items-center justify-center">
 			<InputOTP maxLength={7} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
 				<InputOTPGroup>
-					<InputOTPSlot className="input-star" index={0} />
+					<InputOTPSlot className="input-circle" index={0} />
 
-					<InputOTPSlot className="input-circle" index={1} />
-					<InputOTPSlot className="input-triangle" index={2} />
+					<InputOTPSlot index={1} />
+					<InputOTPSlot className="input-circle" index={2} />
 					<InputOTPSlot index={3} />
-					<InputOTPSlot className="input-star" index={4} />
-					<InputOTPSlot className="input-circle" index={5} />
-					<InputOTPSlot className="input-triangle" index={6} />
+					<InputOTPSlot className="input-circle" index={4} />
+					<InputOTPSlot index={5} />
+					<InputOTPSlot className="input-circle" index={6} />
 				</InputOTPGroup>
 			</InputOTP>
 		</div>
