7411766875c8602abacd0f5b0a288cc21ce3dae4
Tomiwa Adelae <thetommedia@gmail.com>
2024-09-24 08:15:06 +0100

    Updated the colors

--- components/LettersInput.tsx
+++ components/LettersInput.tsx
@@ -10,7 +10,7 @@ import {
 const LettersInput = () => {
 	return (
 		<div className="w-full flex items-center justify-center">
-			<InputOTP maxLength={7}>
+			<InputOTP maxLength={7} pattern={REGEXP_ONLY_DIGITS_AND_CHARS}>
 				<InputOTPGroup>
 					<InputOTPSlot className="input-circle" index={0} />
 
